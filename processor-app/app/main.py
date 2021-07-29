b73f55e6c993112b8a0340f51a5416415a3878ff
blackknightdevelop <blackknightdevelop@users.noreply.github.com>
2021-07-29 10:31:32 +0300

    configuring communication with local mongodb

--- processor-app/app/main.py
+++ processor-app/app/main.py
@@ -5,15 +5,15 @@ import requests
 import time
 import json  
 
+API = 'http://host.docker.internal:8000/update-data/'
+
 config = Config()
 data_fetcher = DataFetcher(config)
 repository = Repository()
 
-URL = 'http://host.docker.internal:8000/update-data/'
-
 while True:
     data = repository.fetch_db();
-    x = requests.post(URL, json = data).json()
+    x = requests.post(API, json = data).json()
     print(x)
 
     # symbols_price = data_fetcher.get_symbols_price()
--- processor-app/app/repository.py
+++ processor-app/app/repository.py
@@ -1,22 +1,42 @@
 from pymongo import MongoClient
 import json
 
+DOMAIN = 'localhost'
+PORT = 27017
+
 class Repository:
 
     def get_db(self):
-        client = MongoClient(
-            host='test_mongodb',
-            port=27017,
-            username='root',
-            password='password',
-            authSource='admin')
-        db = client["currencies_db"]
+        # db = MongoClient(
+        # host = DOMAIN + ":" + str(PORT),
+        # serverSelectionTimeoutMS = 3000, # 3 second timeout
+        # username = "root",
+        # password = "pass")
+
+        # client = MongoClient(
+        #     host='test_mongodb',
+        #     port=27017,
+        #     username='root',
+        #     password='password',
+        #     authSource='admin')
+        # db = client["currencies_db"]
+
+        client = MongoClient(DOMAIN, PORT)
+        db = client['currencies_db']
+        collection = db['currencies']
+        collection.drop()
         return db
 
     def fetch_db(self):
-        print("dddd")
+        print("fetch_db")
         db = self.get_db()
-        data = db.currencies_db.find()
-        currencies = [{"id": item["id"], "name": item["name"]} for item in data]
-        return json.dumps({"currencies": currencies})
+        collection = db['currencies']
+
+        data = {'id': 'j5555', 'name':'44'}
+        collection.insert_one(data)
+
+        ans = [{"id": item["id"], "name": item["name"]} for item in collection.find()]
+        print(ans)
+        return ans
+    
 
